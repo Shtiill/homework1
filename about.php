5d8de68f3754ee25e406f678f06ff169f8c8c9d6
Shtiill <sevinkgr@gmail.com>
2017-02-06 13:22:52 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Алексей - начинающий php-программист</title>
+	<meta charset="utf-8">
+	 <style>
+            body {
+                font-family: sans-serif;  
+            }
+            
+            dl {
+                display: table-row;
+            }
+            
+            dt, dd {
+                display: table-cell;
+                padding: 5px 10px;
+            }
+
+            dt {
+            	font-weight: bold;
+            }
+        </style>
+</head>
+
+<?php 
+	$name = Алексей;
+	$age = 24;
+	$mail = 'sevinkgr@gmail.com';
+	$sity = 'Севастополь';
+	$about = 'Учусь кодить на PHP';
+?>
+
+<body>
+	<h1>Страница пользователя Алексей</h1>
+		<dl>
+            <dt>Имя</dt>
+            <dd><?php echo $name ?></dd>
+     	</dl>
+    	<dl>
+            <dt>Возраст</dt>
+            <dd><?php echo $age ?></dd>
+        </dl>
+        <dl>
+            <dt>Адрес электронной почты</dt>
+            <dd><a href="<?php echo $mail ?>"><?php echo $mail ?></a></dd>
+        </dl>
+        <dl>
+            <dt>Город</dt>
+            <dd><?php echo $sity ?></dd>
+        </dl>
+        <dl>
+            <dt>О себе</dt>
+            <dd><?php echo $about ?></dd>
+        </dl>
+</body>
+</html>
